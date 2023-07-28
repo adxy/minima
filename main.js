01e1eea44fc33809cd86f9baea7b479c8601c4c3
Adarsh <theadxy@gmail.com>
2023-07-28 09:45:53 +0530

    add project section mouse scroll

--- main.js
+++ main.js
@@ -30,6 +30,38 @@ const query = `{
     }
   }`;
 
+// Projects mouse scroll starts.
+const slider = document.getElementById("projects");
+let mouseDown = false;
+let startX, scrollLeft;
+
+let startDragging = function (e) {
+  slider.style.cursor = "grabbing";
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+};
+
+let stopDragging = function (event) {
+  slider.style.cursor = null;
+  mouseDown = false;
+};
+
+slider.addEventListener("mousemove", (e) => {
+  e.preventDefault();
+  if (!mouseDown) {
+    return;
+  }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+});
+
+slider.addEventListener("mousedown", startDragging, false);
+slider.addEventListener("mouseup", stopDragging, false);
+slider.addEventListener("mouseleave", stopDragging, false);
+// Projects mouse scroll ends.
+
 gql(query)
   .then((resp) => {
     const apiResponse = resp.data.user.publication.posts;
